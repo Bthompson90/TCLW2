684c8f8df632848c00219e6698f86a357156d66b
Dakoda Greaves <dakoda@xanimo.net>
2021-06-02 15:40:11 -0700

    adds access control to Box

--- contracts/Box.sol
+++ contracts/Box.sol
@@ -2,19 +2,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract Box {
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
     uint256 private value;
 
-    // Emitted when the stored value changes
     event ValueChanged(uint256 newValue);
 
-    // Stores a new value in the contract
-    function store(uint256 newValue) public {
+    // The onlyOwner modifier restricts who can call the store function
+    function store(uint256 newValue) public onlyOwner {
         value = newValue;
         emit ValueChanged(newValue);
     }
 
-    // Reads the last stored value
     function retrieve() public view returns (uint256) {
         return value;
     }
